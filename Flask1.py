4fd6a6202d258e4908ddd003c1ac6b9069d033ee
Nandini Soni <117187906+iamnandini@users.noreply.github.com>
2023-07-15 00:05:56 +0530

    False to debug

--- Flask1.py
+++ Flask1.py
@@ -62,4 +62,4 @@ def search():
     return render_template("search.html", matching_todo=matching_todo)
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=False,port=5001)
