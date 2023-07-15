1979e668f702bb4ea3119db40bead7034c79ada4
Nandini Soni <117187906+iamnandini@users.noreply.github.com>
2023-07-15 21:11:44 +0530

    ew

--- Flask1.py
+++ Flask1.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
 
-
-
 app = Flask(__name__)
 SQLALCHEMY_DATABASE_URI = "sqlite:///instance/todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
